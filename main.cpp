38cd2085032556395bf7194e4cc5a9710963330b
Leothomasv <leothomasv@unitec.edu>
2022-03-12 16:15:03 -0600

    leer archivo trace

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include "iostream"
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main(){
-    cout << "Prueba";
+int main()
+{
+    string dir;
+    string op;
+    ifstream MyReadFile("gcc.trace");
+    while (getline(MyReadFile, dir))
+    {
+        cout << dir << endl;
+    }
+    MyReadFile.close();
 }
