a29112ff0710d2a355a2b19de045b876e5ca420d
Leothomasv <leothomasv@unitec.edu>
2022-03-12 20:38:39 -0600

    leer datos

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -8,9 +9,12 @@ int main()
     string dir;
     string op;
     ifstream MyReadFile("gcc.trace");
+
     while (getline(MyReadFile, dir))
     {
-        cout << dir << endl;
+        //cout << dir << endl;
+        op = dir[9];
+        cout << op << endl;
     }
     MyReadFile.close();
 }
